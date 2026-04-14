6b36ad265688d09dce747409e9e7a5a3ce9e8861
GodefroyL <godefroy.lecluse@gmail.com>
2026-04-14 04:09:18 +0300

    Deleting of unused files

--- Test_rtde_read.py
+++
@@ -1,8 +0,0 @@
-from rtde_receive import RTDEReceiveInterface as RTDEReceive
-
-ROBOT_IP = "10.159.202.217"
-
-rtde_r = RTDEReceive(ROBOT_IP)
-
-print("Joints :", rtde_r.getActualQ())
-print("TCP pose :", rtde_r.getActualTCPPose())
